c6fe69af2ce3b978ca25a48a2fe48e1e1fabb79b
Michael Harmon <mfh.scoobydoo@gmail.com>
2026-02-25 00:41:34 -0700

    don't repeat yourself

--- pipeline/pipe/m/rig_builder/test/tests/cycle.py
+++ pipeline/pipe/m/rig_builder/test/tests/cycle.py
@@ -1,9 +1,20 @@
-from typing import FrozenSet, Iterable
+from typing import FrozenSet, Iterable, Sequence
 
 from maya import cmds
 
 from .. import RigBuildTest
 from ..common import get_evaluation_manager_nodes
+
+
+def _format_clusters_for_log(clusters: Iterable[Sequence[str]]):
+    cluster_sizes_and_names = ((len(cluster), cluster[0]) for cluster in clusters)
+    cluster_log_strings: list[str] = [
+        f"{cluster_data[1]}: {cluster_data[0]} nodes"
+        for cluster_data in sorted(
+            cluster_sizes_and_names, key=lambda x: x[0], reverse=True
+        )
+    ]
+    return cluster_log_strings
 
 
 class TestLargeCyclesEM(RigBuildTest):
@@ -45,17 +56,10 @@ class TestLargeCyclesEM(RigBuildTest):
             else:
                 processed_nodes.add(node)
 
-        cycle_sizes_and_names = (
-            (len(cluster), cluster[0]) for cluster in large_cycle_clusters
-        )
-
         if large_cycle_clusters:
-            cluster_log_strings: list[str] = [
-                f"{cluster_data[1]}: {cluster_data[0]} nodes"
-                for cluster_data in sorted(
-                    cycle_sizes_and_names, key=lambda x: x[0], reverse=True
-                )
-            ]
+            cluster_log_strings: list[str] = _format_clusters_for_log(
+                large_cycle_clusters
+            )
             formatted_clusters = "\n".join(cluster_log_strings)
             self.log_warn(f"Scene has large EM cluster(s): {formatted_clusters}")
             return False
@@ -85,6 +89,21 @@ def _get_cycles_from_nodes(nodes: Iterable[str], threshold: int = 0) -> list[lis
     return large_cycle_clusters
 
 
+def _run_dg_cycle_test(test_object: RigBuildTest, threshold: int = 0):
+    # Dependency Graph Cycle
+    dg_cycle_nodes: list[str] = cmds.cycleCheck(all=True, list=True) or []  # type: ignore
+
+    large_cycle_clusters = _get_cycles_from_nodes(dg_cycle_nodes, threshold=threshold)
+    if large_cycle_clusters:
+        cluster_log_strings: list[str] = _format_clusters_for_log(large_cycle_clusters)
+        formatted_clusters = "\n".join(cluster_log_strings)
+        test_object.log_warn(f"Scene has large DG cluster(s): {formatted_clusters}")
+        return False
+    else:
+        test_object.log_success()
+        return True
+
+
 class TestLargeCyclesDG(RigBuildTest):
     """
     Checks that the scene has no large dependency graph cycles. Currently the threshold is 10 nodes.
@@ -96,28 +115,7 @@ class TestLargeCyclesDG(RigBuildTest):
         super().__init__("No large cycles (DG)")
 
     def run(self):
-        # Dependency Graph Cycle
-        dg_cycle_nodes: list[str] = cmds.cycleCheck(all=True, list=True) or []  # type: ignore
-
-        large_cycle_clusters = _get_cycles_from_nodes(
-            dg_cycle_nodes, threshold=self.CYCLE_THRESHOLD
-        )
-        cycle_sizes_and_names = (
-            (len(cluster), cluster[0]) for cluster in large_cycle_clusters
-        )
-        if large_cycle_clusters:
-            cluster_log_strings: list[str] = [
-                f"{cluster_data[1]}: {cluster_data[0]} nodes"
-                for cluster_data in sorted(
-                    cycle_sizes_and_names, key=lambda x: x[0], reverse=True
-                )
-            ]
-            formatted_clusters = "\n".join(cluster_log_strings)
-            self.log_warn(f"Scene has large DG cluster(s): {formatted_clusters}")
-            return False
-        else:
-            self.log_success()
-            return True
+        return _run_dg_cycle_test(self, self.CYCLE_THRESHOLD)
 
 
 class TestCyclesDG(RigBuildTest):
@@ -129,26 +127,4 @@ class TestCyclesDG(RigBuildTest):
         super().__init__("No cycles (DG)")
 
     def run(self):
-        # Dependency Graph Cycle
-        has_cycles: bool = cmds.cycleCheck(all=True)  # type: ignore
-
-        if has_cycles:
-            dg_cycle_nodes: list[str] = cmds.cycleCheck(all=True, list=True) or []  # type: ignore
-
-            large_cycle_clusters = _get_cycles_from_nodes(dg_cycle_nodes)
-            cycle_sizes_and_names = (
-                (len(cluster), cluster[0]) for cluster in large_cycle_clusters
-            )
-
-            cluster_log_strings: list[str] = [
-                f"{cluster_data[1]}: {cluster_data[0]} nodes"
-                for cluster_data in sorted(
-                    cycle_sizes_and_names, key=lambda x: x[0], reverse=True
-                )
-            ]
-            formatted_clusters = "\n".join(cluster_log_strings)
-            self.log_warn(f"Scene has DG cycle(s): {formatted_clusters}")
-            return False
-        else:
-            self.log_success()
-            return True
+        return _run_dg_cycle_test(self, 0)
